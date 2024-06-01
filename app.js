9009b5d870fd86565fdd2e5618092524d23e856c
Kaan Danisment <kaandanisment1qgmail.com>
2024-06-01 21:36:12 +0300

    CRUD Operations and Routers completed

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const fileUpload = require("express-fileupload")
+const methodOverride = require('method-override')
 const ejs = require("ejs")
 const path = require("path")
-const Photo = require("./models/Photo")
+const fs = require("fs")
+
+const photoController = require("./contollers/photoController")
+const pageController = require("./contollers/pageController")
 
 const app = express();
 
@@ -15,31 +20,23 @@ app.set("view engine", "ejs")
 // Middlewares
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.json()) 
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+    methods:["POST","GET"]
+}))
 
 // ROUTES
-app.get("/", async (req,res) => {
-    const photos = await Photo.find({})
-    res.render("index", {
-        photos
-    })
-})
-app.get("/about",(req,res) => {
-    res.render("about")
-})
-app.get("/add",(req,res) => {
-    res.render("add")
-})
-app.get("/photos/:id", async(req,res) =>{
-    const photo = await (Photo.findById(req.params.id))
-    res.render("photo", {
-        photo
-    })
-})
-app.post("/photos", async (req,res) => {
-    await Photo.create(req.body)
-    res.redirect("/")
-})
+app.get("/about",pageController.getAboutPAge)
+app.get("/add",pageController.getAddPage)
+app.get("/photos/edit/:id", pageController.getEditPage)
+
+app.get("/", photoController.getAllPhotos)
+app.get("/photos/:id", photoController.getPhoto)
+app.put("/photos/:id", photoController.updatePhoto)
+app.post("/photos", photoController.createPhoto)
+app.delete("/photos/:id", photoController.deletePhoto)
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server started in port ${port} `)
